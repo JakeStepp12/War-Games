2595b227039c9d21afae2d67a400f123ca0585b7
Jake Stepp <jakeallenstepp@gmail.com>
2023-06-21 03:45:41 -0500

    Added Global-Thermonuclear-War.py and allowed WOPR to launch GTW

--- War Games/Global-Thermonuclear-War.py
+++ War Games/Global-Thermonuclear-War.py
@@ -1,17 +1,65 @@
-print("                            GLOBAL THERMONUCLEAR WAR\n\n")
-print("      ___    ____             ____                   _______________")
-print("     |   \\__/    \\_____      /    |              ___/               \\")
-print("     |                 \\    /    /           ___/                    \\______")
-print("     |                  \\__/    /           /                               \\")
-print("     |                           \\        _/                                 |")
-print("     |        UNITED STATES       |      /           SOVIET UNION       ____/")
-print("      \\                           /     /                          ____/")
-print("        \\                        /     |                          /")
-print("          \\________          __/       \\          _____   /\\_    /")
-print("                   \\__      /            \\__    _/     \\_/   \\__/")
-print("                      \\____/                \\__/")
-print("\n");
-print("WHICH SIDE DO YOU WANT?\n\n")
-print("  1. UNITED STATES\n")
-print("  2. SOVIET UNION\n\n")
+from tkinter import *
+
+root = Tk()
+
+#Creating a Label Widget
+myLabel1 = Label(root, text="                            GLOBAL THERMONUCLEAR WAR")
+myLabel2 = Label(root, text="      ___    ____             ____                   _______________")
+myLabel3 = Label(root, text="     |   \\__/    \\_____      /    |              ___/               \\")
+myLabel4 = Label(root, text="     |                 \\    /    /           ___/                    \\______")
+myLabel5 = Label(root, text="     |                  \\__/    /           /                               \\")
+myLabel6 = Label(root, text="     |                           \\        _/                                 |")
+myLabel7 = Label(root, text="     |        UNITED STATES       |      /           SOVIET UNION       ____/")
+myLabel8 = Label(root, text="      \\                           /     /                          ____/")
+myLabel9 = Label(root, text="        \\                        /     |                          /")
+myLabel10 = Label(root, text="          \\________          __/       \\          _____   /\\_    /")
+myLabel11 = Label(root, text="                   \\__      /            \\__    _/     \\_/   \\__/")
+myLabel12 = Label(root, text="                      \\____/                \\__/")
+myLabel13 = Label(root, text="                                                                                     ")
+myLabel14 = Label(root, text="WHICH SIDE DO YOU WANT?")
+myLabel15 = Label(root, text="  1. UNITED STATES")
+myLabel16 = Label(root, text="  2. SOVIET UNION")
+
+#Shoving it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+myLabel3.grid(row=2, column=0)
+myLabel4.grid(row=3, column=0)
+myLabel5.grid(row=4, column=0)
+myLabel6.grid(row=5, column=0)
+myLabel7.grid(row=6, column=0)
+myLabel8.grid(row=7, column=0)
+myLabel9.grid(row=8, column=0)
+myLabel10.grid(row=9, column=0)
+myLabel11.grid(row=10, column=0)
+myLabel12.grid(row=11, column=0)
+myLabel13.grid(row=12, column=0)
+myLabel14.grid(row=13, column=0)
+myLabel15.grid(row=14, column=0)
+myLabel16.grid(row=15, column=0)
+
+
+
+#Creating an Event Loop
+root.mainloop()
+
+
+
+
+#print("                            GLOBAL THERMONUCLEAR WAR\n\n")
+#print("      ___    ____             ____                   _______________")
+#print("     |   \\__/    \\_____      /    |              ___/               \\")
+#print("     |                 \\    /    /           ___/                    \\______")
+#print("     |                  \\__/    /           /                               \\")
+#print("     |                           \\        _/                                 |")
+#print("     |        UNITED STATES       |      /           SOVIET UNION       ____/")
+#print("      \\                           /     /                          ____/")
+#print("        \\                        /     |                          /")
+#print("          \\________          __/       \\          _____   /\\_    /")
+#print("                   \\__      /            \\__    _/     \\_/   \\__/")
+#print("                      \\____/                \\__/")
+#print("\n");
+#print("WHICH SIDE DO YOU WANT?\n\n")
+#print("  1. UNITED STATES\n")
+#print("  2. SOVIET UNION\n\n")
 #side = int(input("PLEASE CHOOSE ONE: "))
